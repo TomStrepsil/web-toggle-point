a46f3d1ab20bfa51c7a8dcd750f89e8cdb1887b8
Tom Pereira <10725179+TomStrepsil@users.noreply.github.com>
2025-03-16 19:29:11 +0000

    update join point generation, and add options to schema

--- packages/webpack/src/plugins/togglePointInjection/setupSchemeModules/generateJoinPoint.js
+++ packages/webpack/src/plugins/togglePointInjection/setupSchemeModules/generateJoinPoint.js
@@ -1,19 +1,46 @@
-import { posix } from "path";
-import regexgen from "regexgen";
 import { POINT_CUTS, SCHEME } from "../constants.js";
-const { dirname } = posix;
+
+const getStatic = ({ path, variants }) => {
+  const variantsKeys = Array.from(variants.keys());
+  const code = `import * as joinPoint from "${path}";
+${variantsKeys
+  .map(
+    (key, index) => `import * as variant_${index} from "${variants.get(key)}";`
+  )
+  .join("\n")}
+const variants = new Map([
+${variantsKeys.map((relativePath, index) => `  ["${relativePath}", variant_${index}]`).join(",\n")}
+]);`;
+
+  return code;
+};
+
+const getDynamic = (method, webpackMagicComment, { path, variants }) => {
+  const variantsKeys = Array.from(variants.keys());
+  const code = `const joinPoint = () => ${method}(${webpackMagicComment}"${path}");
+const variants = new Map([
+${variantsKeys.map((key) => `  ["${key}", () => ${method}(${webpackMagicComment}"${variants.get(key)}")]`).join(",\n")}
+]);`;
+
+  return code;
+};
+
+const getDynamicRequire = getDynamic.bind(undefined, "require");
+const getDynamicImport = getDynamic.bind(undefined, "import");
 
 const generateJoinPoint = ({ joinPointFiles, path }) => {
   const {
-    pointCut: { name },
+    pointCut: { name, loadingMode, webpackMagicComment },
     variants
   } = joinPointFiles.get(path);
-  const directory = dirname(path);
-  const regex = regexgen(variants);
-
-  return `import pointCut from "${SCHEME}:${POINT_CUTS}:/${name}";
-import * as joinPoint from "${path}";
-const variants = import.meta.webpackContext("${directory}", { recursive: true, regExp: ${regex} });
+  const pointCutImport = `import pointCut from "${SCHEME}:${POINT_CUTS}:/${name}";`;
+  const code = {
+    dynamicImport: getDynamicImport.bind(undefined, webpackMagicComment),
+    dynamicRequire: getDynamicRequire.bind(undefined, ""),
+    static: getStatic
+  }[loadingMode]({ path, variants, webpackMagicComment });
+  return `${pointCutImport}
+${code}
 export default pointCut({ joinPoint, variants });`;
 };
 
--- packages/webpack/src/plugins/togglePointInjection/setupSchemeModules/generateJoinPoint.test.js
+++ packages/webpack/src/plugins/togglePointInjection/setupSchemeModules/generateJoinPoint.test.js
@@ -7,41 +7,140 @@ jest.mock("../constants", () => ({
 }));
 
 describe("generateJoinPoint", () => {
-  const path = "test-folder/test-path";
+  const path = "/test-folder/test-path";
   const pointCutName = "test-point-cut";
-  const variants = [
-    "test-sub-folder/test-variant-1",
-    "test-sub-folder/test-variant-2",
-    "test-other-sub-folder/test-variant-1"
+  const relativePaths = [
+    "/test-sub-folder/test-variant-1",
+    "/test-sub-folder/test-variant-2",
+    "/test-other-sub-folder/test-variant-1"
   ];
+  const variants = new Map(
+    relativePaths.map((relativePath) => [
+      relativePath,
+      `${path}${relativePath}`
+    ])
+  );
+  const pointCut = { name: pointCutName };
   let result;
 
-  beforeEach(() => {
-    const joinPointFiles = new Map([
-      [path, { pointCut: { name: pointCutName }, variants }]
-    ]);
-    result = generateJoinPoint({ joinPointFiles, path });
+  const makeCommonAssertions = () => {
+    it("should return a script that imports the appropriate point cut for the join point", () => {
+      expect(result).toMatch(
+        `import pointCut from "${SCHEME}:${POINT_CUTS}:/${pointCutName}";`
+      );
+    });
+
+    it("should return a script exports a default export which calls the point cut, passing the join point (control module) and the variants", () => {
+      expect(result).toMatch(
+        "export default pointCut({ joinPoint, variants });"
+      );
+    });
+  };
+
+  describe("with a static loadingMode", () => {
+    beforeEach(() => {
+      const joinPointFiles = new Map([
+        [path, { pointCut: { ...pointCut, loadingMode: "static" }, variants }]
+      ]);
+      result = generateJoinPoint({
+        joinPointFiles,
+        path
+      });
+    });
+
+    makeCommonAssertions();
+
+    it("should return a script that statically imports the base / control module for the join point", () => {
+      expect(result).toMatch(`import * as joinPoint from "${path}";`);
+    });
+
+    it("should return a script that imports all the valid variants of the base / control module, storing in variables", () => {
+      expect(result).toMatch(`
+import * as variant_0 from "${path}${relativePaths[0]}";
+import * as variant_1 from "${path}${relativePaths[1]}";
+import * as variant_2 from "${path}${relativePaths[2]}";`);
+    });
+
+    it("should return a script that creates a Map of variants, keyed by relative path, valued as the variant module", () => {
+      expect(result).toMatch(`const variants = new Map([
+  ["/test-sub-folder/test-variant-1", variant_0],
+  ["/test-sub-folder/test-variant-2", variant_1],
+  ["/test-other-sub-folder/test-variant-1", variant_2]
+]);`);
+    });
   });
 
-  it("should return a script that imports the appropriate point cut for the join point", () => {
-    expect(result).toMatch(
-      `import pointCut from "${SCHEME}:${POINT_CUTS}:/${pointCutName}";`
-    );
+  describe("with a dynamicImport loadingMode", () => {
+    const webpackMagicComment = "/* test loading qualifier */";
+    beforeEach(() => {
+      const joinPointFiles = new Map([
+        [
+          path,
+          {
+            pointCut: {
+              ...pointCut,
+              loadingMode: "dynamicImport",
+              webpackMagicComment
+            },
+            variants
+          }
+        ]
+      ]);
+      result = generateJoinPoint({
+        joinPointFiles,
+        path
+      });
+    });
+
+    makeCommonAssertions();
+
+    it("should return a script that prepares a join point function that will dynamically load the join point, when executed, with any provided webpack loading directives", () => {
+      expect(result).toMatch(
+        `const joinPoint = () => import(${webpackMagicComment}"${path}");`
+      );
+    });
+
+    it("should return a script that creates a Map of variants, keyed by relative path, valued as a function that will dynamically load the variant module when executed, with any provided webpack loading directives", () => {
+      expect(result).toMatch(`const variants = new Map([
+  ["/test-sub-folder/test-variant-1", () => import(${webpackMagicComment}"${path}${relativePaths[0]}")],
+  ["/test-sub-folder/test-variant-2", () => import(${webpackMagicComment}"${path}${relativePaths[1]}")],
+  ["/test-other-sub-folder/test-variant-1", () => import(${webpackMagicComment}"${path}${relativePaths[2]}")]
+]);`);
+    });
   });
 
-  it("should return a script that imports the base / control module for the join point", () => {
-    expect(result).toMatch(`import * as joinPoint from "${path}";`);
-  });
+  describe("with a dynamicRequire loadingMode", () => {
+    beforeEach(() => {
+      const joinPointFiles = new Map([
+        [
+          path,
+          {
+            pointCut: {
+              ...pointCut,
+              loadingMode: "dynamicRequire"
+            },
+            variants
+          }
+        ]
+      ]);
+      result = generateJoinPoint({
+        joinPointFiles,
+        path
+      });
+    });
 
-  it("should return a script that imports all the valid variants of the base / control module into a webpackContext, using a minimal regex that matches all the variants", () => {
-    expect(result).toMatch(
-      `const variants = import.meta.webpackContext("${
-        path.split("/")[0]
-      }", { recursive: true, regExp: /test\\-(?:other\\-sub\\-folder\\/test\\-variant\\-1|sub\\-folder\\/test\\-variant\\-[12])/ });`
-    );
-  });
+    makeCommonAssertions();
 
-  it("should return a script exports a default export which calls the point cut, passing the join point (control module) and the variants", () => {
-    expect(result).toMatch("export default pointCut({ joinPoint, variants });");
+    it("should return a script that prepares a join point function that will dynamically load the join point, when executed", () => {
+      expect(result).toMatch(`const joinPoint = () => require("${path}");`);
+    });
+
+    it("should return a script that creates a Map of variants, keyed by relative path, valued as a function that will dynamically load the variant module when executed", () => {
+      expect(result).toMatch(`const variants = new Map([
+  ["/test-sub-folder/test-variant-1", () => require("${path}${relativePaths[0]}")],
+  ["/test-sub-folder/test-variant-2", () => require("${path}${relativePaths[1]}")],
+  ["/test-other-sub-folder/test-variant-1", () => require("${path}${relativePaths[2]}")]
+]);`);
+    });
   });
 });
