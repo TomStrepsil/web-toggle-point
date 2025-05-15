8d3d0954a93ce2ad32bc80a92789d64d6b4d0b3a
TomStrepsil <10725179+TomStrepsil@users.noreply.github.com>
2025-05-15 23:18:56 +0100

    fixup schema for loadStrategy

--- packages/webpack/src/index.js
+++ packages/webpack/src/index.js
@@ -5,3 +5,9 @@ import "./external.js";
  * @module web-toggle-point-webpack
  */
 export { TogglePointInjectionPlugin } from "./plugins";
+
+/**
+ * @interface loadStrategy
+ * @description A module load strategy. This is used to load the modules that contain the join point module and its variants
+ * @memberof module:web-toggle-point-webpack
+ */
