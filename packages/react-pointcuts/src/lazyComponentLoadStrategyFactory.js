261ecf4077fcd031d9cc4fc5344ab816e11d6d7f
TomStrepsil <10725179+TomStrepsil@users.noreply.github.com>
2025-05-18 13:31:37 +0100

    JSDoc

--- packages/react-pointcuts/src/lazyComponentLoadStrategyFactory.js
+++ packages/react-pointcuts/src/lazyComponentLoadStrategyFactory.js
@@ -7,10 +7,10 @@ const adapterModuleSpecifier = import.meta.filename;
 export const pack = (expression) => lazy(expression);
 
 /**
- * A component load strategy factory to generate a load strategy using React's lazy function
+ * A component load strategy factory to generate a load strategy using React's lazy function. Wraps the {@link module:web-toggle-point-webpack.deferredDynamicImportLoadStrategyFactory|deferredDynamicImportLoadStrategyFactory} from the Webpack package
  * @memberof module:web-toggle-point-react-pointcuts
  * @param {object} [options] options
- * @param {object} [options.importCodeGeneratorFactoryOptions] options for the code generator factory // TODO: interface referenced in webpack package?
+ * @param {object} [options.importCodeGeneratorFactoryOptions] options for the code generator factory. see {@link module:web-toggle-point-webpack.deferredDynamicImportLoadStrategyFactory|deferredDynamicImportLoadStrategyFactory}
  * @param {string} [options.importCodeGeneratorFactoryOptions.webpackMagicComment] a magic comment prefixing the {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/import|dynamic import} statement. see {@link https://webpack.js.org/api/module-methods/#magic-comments|Webpack Magic Comments}
  * @returns {module:web-toggle-point-webpack.loadStrategy} a load strategy
  * @see {@link https://reactjs.org/docs/code-splitting.html#reactlazy|React.lazy}
