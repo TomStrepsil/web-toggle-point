04d88fbb1a100ee11f746b2eed38b632d1a72867
TomStrepsil <10725179+TomStrepsil@users.noreply.github.com>
2025-05-20 18:33:28 +0100

    update package.lock ensure import.meta.filename works for commonjs output of react pointcuts package

--- packages/react-pointcuts/build/rollup.mjs
+++ packages/react-pointcuts/build/rollup.mjs
@@ -4,45 +4,62 @@ import external from "rollup-plugin-auto-external";
 import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
 import keepExternalComments from "./keepExternalComments.mjs";
+import replace from "@rollup/plugin-replace";
 
-export default ({ config_isClient }) => {
-  const CLIENT = JSON.parse(config_isClient);
-  return {
-    input: {
-      main: "./src/index.js",
-      lazyComponentLoadStrategyFactory:
-        "./src/lazyComponentLoadStrategyFactory.js"
-    },
-    output: [
-      {
+const getCommon = ({ config_isClient }) => ({
+  input: {
+    main: "./src/index.js",
+    lazyComponentLoadStrategyFactory:
+      "./src/lazyComponentLoadStrategyFactory.js"
+  },
+  external: ["react/jsx-runtime"],
+  plugins: [
+    keepExternalComments,
+    babel({
+      exclude: [/node_modules/],
+      babelHelpers: "runtime"
+    }),
+    resolve({
+      preferBuiltins: true
+    }),
+    commonjs(),
+    external(),
+    ...[JSON.parse(config_isClient) ? terser() : []]
+  ],
+  preserveSymlinks: true
+});
+
+export default (args) => {
+  const common = getCommon(args);
+  return [
+    {
+      ...common,
+      output: {
         dir: "lib/",
         exports: "named",
         format: "es",
         entryFileNames: "[name].js",
         sourcemap: true
-      },
-      {
+      }
+    },
+    {
+      ...common,
+      output: {
         dir: "lib/",
         exports: "named",
         format: "cjs",
         entryFileNames: "[name].es5.cjs",
         sourcemap: true
-      }
-    ],
-    external: ["react/jsx-runtime"],
-    plugins: [
-      keepExternalComments,
-      babel({
-        exclude: [/node_modules/],
-        babelHelpers: "runtime"
-      }),
-      resolve({
-        preferBuiltins: true
-      }),
-      commonjs(),
-      external(),
-      ...[CLIENT ? terser() : []]
-    ],
-    preserveSymlinks: true
-  };
+      },
+      plugins: [
+        ...common.plugins,
+        replace({
+          preventAssignment: true,
+          values: {
+            "import.meta.filename": "__filename"
+          }
+        })
+      ]
+    }
+  ];
 };
